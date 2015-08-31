98339bf1f97363785cd3d052cbdb38ea6ba5d943
nacnudus <nacnudus@gmail.com>
2015-08-31 21:10:51 +1200

    Add setup for Arch Linux

---
+++ arch.sh
@@ -0,0 +1,271 @@
+# To verify the torrent download, you need to import the key.
+gpg --keyserver pgpkeys.mit.edu --recv-key 9741E8AC
+gpg --verify archlinux-2015.08.01-dual.iso.sig
+
+# Do the beginners' guide.
+
+# If grub-install complains about EFI, then force it to use BIOS by specifying --target-i386-pc
+
+# The wireless network device is called something like wlp2s0
+
+# After following the beginners' guide, if it doesn't boot, then try this from 
+# http://unix.stackexchange.com/a/152277/46012
+
+# "I ran across the same issue just now, and found another workaround. Basically, it involves making the hosts /run directory available to the guest.
+
+# First, we mount /run where it can be accessed by the guest. I will assume that your install partition is mounted at /mnt
+
+mkdir /mnt/hostrun
+mount --bind /run /mnt/hostrun
+
+# Then, we chroot into the guest, and mount our host's /run/lvm in the guest's /run
+
+arch-chroot /mnt /bin/bash
+mkdir /run/lvm
+mount --bind /hostrun/lvm /run/lvm
+
+# You can then run grub-mkconfig and grub-install without any LVM errors. This also makes the commands behave if you are installing with LVM, for what it's worth.
+# When done, remember to umount /run/lvm before exiting the chroot."
+
+
+
+# Create a user
+useradd -m -G wheel nacnudus
+chfn nacnudus
+passwd nacnudus
+
+# Install sudo
+pacman -S sudo
+visudo
+# add the line
+nacnudus ALL=(ALL) ALL
+exit
+# login as nacnudus
+
+# Allow colour in pacman
+sudo vi /etc/pacman.conf
+# and uncomment the 'Color' line
+
+# Install virtualbox guest additions (if a guest in a virtualbox host)
+sudo pacman -S virtualbox-guest-utils
+# Choose the default mesa library at the prompt
+sudo systemctl enable vboxservice
+
+# Install X, i3, and gnome-terminal (TODO: video acceleration? mesa-vdpau and libva-mesa-driver)
+sudo pacman -S xorg-server
+# That doesn't seem to be necessary if you have already installed virtualbox
+sudo pacman -S xorg-xinit
+sudo pacman -S i3
+# Accept all defaults
+sudo pacman -S dmenu
+sudo pacman -S gnome-terminal 
+cp /etc/X11/xinit/xinitrc ~/.xinitrc 
+# Comment out the lines from twm onwards, then add
+exec i3
+# Unless in virtualbox, in which case
+# create ~/.xinitrc with the following lines
+#! /bin/bash
+/usr/sbin/VBoxClient-all
+exec i3
+# Accept all defaults
+startx
+# if the screen is tiny, do
+xrandr 1024x768
+# or some other mode listed by xrandr
+# Make the changes in visudo suggested in ~/.i3/config
+
+# Install python (note, python3 by default, otherwise use python2)
+sudo pacman -S python python-virtualenv python-virtualenvwrapper python-pip
+# Change the line in .zshrc to
+source /usr/bin/virtualenvwrapper.sh
+
+# Install zsh
+sudo pacman -S zsh
+chsh -l
+chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Link to the dotfiles version of .zshrc
+mv .zshrc .zshrc~
+ln -s dotfiles/.zshrc .zshrc
+
+# mount ntfs
+sudo pacman -S ntfs-3g
+
+# battery life (conflicst with pm-utils)
+# sudo pacman -S tlp
+# sudo tlp start
+# sudo systemctl enable tlp.service
+# sudo systemctl enable tlp-sleep.service
+
+# yaourt for unofficial packages
+# commented lines don't seem to work, or be necessary
+sudo pacman -S wget
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
+tar xvf package-query.tar.gz
+cd package-query
+makepkg -sri
+# sudo pacman -U package-query
+tar xvf yaourt.tar.gz
+cd yaourt
+makepkg -sri
+# sudo pacman -U yaourt
+
+# install chruby
+yaourt -S chruby
+# Change the lines in zshrc to
+source /usr/share/chruby/chruby.sh
+source /usr/share/chruby/auto.sh
+
+# Google Chrome
+yaourt -S google-chrome
+# I selected 6 for droid ttf
+sudo pacman -S ttf-liberation
+
+# git
+sudo pacman -S git
+
+# Vim
+sudo pacman -S gvim
+# This comes with dependencies for python2 and lua .  Plain vim doesn't come
+# with clientserver support (no --servername argument for vim-R-plugin or atp),
+# and while you could compile it yourself using the --with-x=yes flag, it's
+# easier just to install gvim.  You can still do `vim` in the terminal, and you
+# won't notice the difference.
+# If there's a problem with missing help files or syntax, try reinstalling
+# the vim-runtime package.
+# Start vim, and do :PlugInstall
+# Now you need fonts for powerline
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+# And restart the terminal
+
+# Shared folders
+sudo gpasswd -a $USER vboxsf
+# now you find it in /media/sf_share
+
+# Install R stats
+sudo pacman -S tmux
+yaourt -s gdal
+yaourt -s openblas
+# I chose option 3 aur/openblas
+yaourt -s intel-mkl
+sudo pacman -S r-mkl
+ln -s dotfiles/.Rprofile .Rprofile
+# Inside R
+install.packages("devtools")
+install.packages("setwidth")
+devtools::install_github("jalvesaq/VimCom")
+devtools::install_github("jalvesaq/colorout")
+devtools::install_github("Data-Camp/Rdocumentation")
+install.packages("rgdal")
+# Rstudo
+yaourt -S rstudio-desktop-bin
+
+# Install ag
+sudo pacman -S the_silver_searcher
+
+# Install TexLive
+sudo mkdir -p /media/iso
+sudo mkdir scripts 
+sudo mount -t iso9660 -o ro,loop,noauto ~/installables/texlive2015-20150523.iso /media/iso
+sudo /media/iso/install-tl --profile=~/scripts/texlive.profile
+export PATH=$PATH:/usr/local/texlive/2015/bin/x86_64-linux
+export MANPATH=$MANPATH:/usr/local/texlive/2015/texmf-dist/doc/man
+export INFOPATH=$INFOPATH:/usr/local/texlive/2015/texmf-dist/doc/info
+sudo umount /media/iso
+sudo tlmgr option repository http://mirror.ctan.org/systems/texlive/tlnet
+sudo tlmgr update --self
+sudo tlmgr update --all
+
+# Install bluetooth
+sudo pacman -S bluez bluez-utils
+sudo modprobe btusb
+sudo systemctl enable bluetooth.service
+# Follow further instructions
+
+# Install mouse
+sudo pacman -S xf86-input-synaptics
+sudo cp /usr/share/X11/xorg.conf.d/50-synaptics.conf /etc/X11/xorg.conf.d
+# Edit the file and write with :w!! for sudo permissions, using settings
+# suggested in the Arch wiki for synaptics.
+
+# Install fonts
+# Take your pick from ttf-anything
+sudo pacman -S ttf-
+
+# Dim backlight
+sudo pacman -S xorg-xbacklight
+sudo pacman -S xf86-video-intel
+# Create file /etc/X11/xorg.conf.d/20-intel.conf with the following lines:
+Section "Device"
+        Identifier  "card0"
+        Driver      "intel"
+        Option      "Backlight"  "intel_backlight"
+        BusID       "PCI:0:2:0"
+EndSection
+
+
+# Directory sizes
+sudo pacman -S baobab
+
+# Backups (back in time)
+gpg --keyserver pgp.mit.edu --recv-key 944B4826
+yaourt -S backintime
+# Run with
+backintime-qt4
+
+# xrandr
+sudo pacman -S xorg-xrandr
+
+# sound
+sudo pacman -S alsa-utils
+# Create ~/.asoundrc with the line
+defaults.ctl.card 1
+defaults.pcm.card 1
+# Check with
+speaker-check
+
+# vlc
+sudo pacman -S vlc
+
+# sleep and hibernate
+sudo pacman -S pm-utils
+
+# blacklist bluetooth to attempt to fix wifi drops
+# create a file /etc/modprobe.d/bluetooth.conf
+blacklist btusb
+blacklist bluetooth
+
+# Let arch devs know about your packages
+sudo pacman -S pkgstats
+
+# Virtualbox
+sudo pacman -S virtualbox virtualbox-guest-utils net-tools
+# Create the file /etc/modules-load.d/virtualbox.conf with the line
+vboxdrv
+vboxnetadp
+vboxnetflt
+vboxpci
+# For usb sharing, use this:
+sudo gpasswd -a $USER vboxusers
+# For webcam etc.
+yaourt -S virtualbox-ext-oracle
+
+# Map alt-key to right-click context menu
+sudo pacman -S xorg-xmodmap
+# use sudo showkey to find the keycode
+xmodmap -pke > ~/.Xmodmap # warning, wipes file if it exists!
+vim ~/.Xmodmap # and do, e.g.
+keycode 100 = Menu NoSymbol Menu
+# in ~/.xinitrc add
+if [ -f $HOME/.Xmodmap  ]; then
+    /usr/bin/xmodmap $HOME/.Xmodmap
+fi
+
+# Calibre ebook library
+sudo pacman -S calibre
+
+# TODO:
+# Cairo
